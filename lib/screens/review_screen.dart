455ea0d42027b634a262554b52d6a4fffdbf9e90
Kathiravan Sekar <kathiravanrs@gmail.com>
2023-05-04 12:41:06 -0400

    tabview(not working as intended)

--- lib/screens/review_screen.dart
+++ lib/screens/review_screen.dart
@@ -54,83 +54,84 @@ class _ReviewScreenState extends State<ReviewScreen> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Expanded(
-              child: buildFriendListView(),
-            ),
+            friendsList(),
             Text(selectedFriend.name),
-            Expanded(
-              child: buildItemListForFriend(),
-            )
+            // itemWidgetListForFriend()
+            tabView(),
           ],
         ),
       ),
     );
   }
 
-  ListView buildItemListForFriend() {
-    return ListView.builder(
-      itemCount: itemListForFriend.length,
-      itemBuilder: (BuildContext ctx, int index) {
-        return Card(
-          elevation: 0,
-          shape: const RoundedRectangleBorder(
-            side: BorderSide(),
-            borderRadius: BorderRadius.all(Radius.circular(12)),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        itemListForFriend[index].name,
-                        style: const TextStyle(fontSize: 18),
+  Widget friendsList() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: friends.length,
+        itemBuilder: (BuildContext context, int index) {
+          Friend friend = friends[index];
+          return InkWell(
+            onTap: () {
+              setState(() {
+                selectedFriend = friends[index];
+                generateList();
+              });
+            },
+            child: Card(
+              elevation: 0,
+              shape: const RoundedRectangleBorder(
+                side: BorderSide(),
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            friend.name,
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                          Text(
+                            "Pre Tax: ${friendPreTaxSplit[friend]!.toStringAsFixed(2)}",
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                          Text(
+                            "Fees: ${friendTaxSplit[friend]!.toStringAsFixed(2)}",
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                        ],
                       ),
-                      Text(
-                        "Total: \$${itemListForFriend[index].getTotalAfterTax().toStringAsFixed(2)}",
-                        style: const TextStyle(fontSize: 12),
-                      ),
-                      SizedBox(
-                        height: 40,
-                        child: buildChipListForItem(index),
-                      )
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    "\$${(itemListForFriend[index].getTotalAfterTax() / (itemFriendMap[itemListForFriend[index]]?.toList().length ?? 1)).toStringAsFixed(2)}",
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
                     ),
-                  ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        "\$${(friendPreTaxSplit[friend]! + friendTaxSplit[friend]!).toStringAsFixed(2)}",
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 
-  ListView buildFriendListView() {
-    return ListView.builder(
-      itemCount: friends.length,
-      itemBuilder: (BuildContext context, int index) {
-        Friend friend = friends[index];
-        return InkWell(
-          onTap: () {
-            setState(() {
-              selectedFriend = friends[index];
-              generateList();
-            });
-          },
-          child: Card(
+  Widget itemWidgetListForFriend() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: itemListForFriend.length,
+        itemBuilder: (BuildContext ctx, int index) {
+          return Card(
             elevation: 0,
             shape: const RoundedRectangleBorder(
               side: BorderSide(),
@@ -145,24 +146,24 @@ class _ReviewScreenState extends State<ReviewScreen> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          friend.name,
-                          style: const TextStyle(fontSize: 20),
+                          itemListForFriend[index].name,
+                          style: const TextStyle(fontSize: 18),
                         ),
                         Text(
-                          "Pre Tax: ${friendPreTaxSplit[friend]!.toStringAsFixed(2)}",
-                          style: const TextStyle(fontSize: 14),
+                          "Total: \$${itemListForFriend[index].getTotalAfterTax().toStringAsFixed(2)}",
+                          style: const TextStyle(fontSize: 12),
                         ),
-                        Text(
-                          "Fees: ${friendTaxSplit[friend]!.toStringAsFixed(2)}",
-                          style: const TextStyle(fontSize: 14),
-                        ),
+                        SizedBox(
+                          height: 20,
+                          child: buildChipListForItem(index),
+                        )
                       ],
                     ),
                   ),
                   Align(
                     alignment: Alignment.centerRight,
                     child: Text(
-                      "\$${(friendPreTaxSplit[friend]! + friendTaxSplit[friend]!).toStringAsFixed(2)}",
+                      "\$${(itemListForFriend[index].getTotalAfterTax() / (itemFriendMap[itemListForFriend[index]]?.toList().length ?? 1)).toStringAsFixed(2)}",
                       style: const TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
@@ -172,9 +173,88 @@ class _ReviewScreenState extends State<ReviewScreen> {
                 ],
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget itemWidgetListForFriendf(Friend f) {
+    setState(() {
+      selectedFriend = f;
+    });
+    return Expanded(
+      child: ListView.builder(
+        itemCount: itemListForFriend.length,
+        itemBuilder: (BuildContext ctx, int index) {
+          return Card(
+            elevation: 0,
+            shape: const RoundedRectangleBorder(
+              side: BorderSide(),
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          itemListForFriend[index].name,
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                          "Total: \$${itemListForFriend[index].getTotalAfterTax().toStringAsFixed(2)}",
+                          style: const TextStyle(fontSize: 12),
+                        ),
+                        SizedBox(
+                          height: 20,
+                          child: buildChipListForItem(index),
+                        )
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "\$${(itemListForFriend[index].getTotalAfterTax() / (itemFriendMap[itemListForFriend[index]]?.toList().length ?? 1)).toStringAsFixed(2)}",
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget tabView() {
+    return Expanded(
+      child: DefaultTabController(
+        length: friends.length,
+        child: Column(
+          children: [
+            TabBar(
+              isScrollable: true,
+              tabs: friends.map((Friend f) => Tab(text: f.name)).toList(),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: friends.map((Friend f) {
+                  return itemWidgetListForFriendf(f);
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -187,8 +267,18 @@ class _ReviewScreenState extends State<ReviewScreen> {
             itemFriendMap[itemListForFriend[index]]?.toList() ?? [];
         return Padding(
           padding: const EdgeInsets.only(right: 4.0),
-          child: Chip(
-              label: Text(l[i].name, style: const TextStyle(fontSize: 10))),
+          child: Container(
+            // color: Theme.of(context).dividerColor,
+            decoration: BoxDecoration(
+              color: Theme.of(context).hoverColor,
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(width: 0.5),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Text(l[i].name, style: const TextStyle(fontSize: 10)),
+            ),
+          ),
         );
       },
     );
